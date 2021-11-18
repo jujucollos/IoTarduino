f95b752a486b87289153d03b5f26eb0129f122d4
jujucollos <68745924+jujucollos@users.noreply.github.com>
2021-11-18 19:10:53 +0100

    Add files via upload

--- Main.ino
+++ Main.ino
@@ -10,12 +10,18 @@ pinMode(led, OUTPUT);
 
 
 void loop(){
+int val_temp_linear;
+int dat_temp_linear;
 Serial.print("Moisture Sensor Value:");
 val = analogRead(analogPin); //read and assign analog value to variable ’val’ 
 data = val; //variable ’val’ assigns value to variable ‘data’
 Serial.println(analogRead(0));
 Serial.print("Water Level Value:");
 Serial.println(data); //print variable ‘data’ by Serial.print
+val_temp_linear=analogRead(2);//Connect LM35 on Analog 2
+dat_temp_linear=(500 * val_temps_linear) /1024;
+Serial.print("Temp:"); //Display the temperature on Serial monitor Serial.print(dat);
+Serial.println("C");
 
 delay(1000);
 }
