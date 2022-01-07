8de7fbc37978f6e569bb9e1bdc5bf45b37c7ca77
jujucollos <68745924+jujucollos@users.noreply.github.com>
2022-01-07 16:02:51 +0100

    Update ESPtest.ino
    [ESP]: set up our values on the right field

--- ESPtest.ino
+++ ESPtest.ino
@@ -9,61 +9,65 @@
 #endif
 #include <InfluxDbClient.h> // load the client library
 #include <InfluxDbCloud.h> // only for InfluxDB Cloud: load SSL certificate and additional method call
-#include "secrets.h" // load connection credentials
+#include "secrets.h"
 
-#define SLEEP_S 30 // how many seconds to sleep between readings
-#define DEVICE_ID "myroom"
+#define SLEEP_S 30
+#define DEVICE_ID "MyRoom"
 
 // InfluxDB client for InfluxDB Cloud API
 InfluxDBClient client_cloud(INFLUXDB_CLOUD_URL, INFLUXDB_CLOUD_ORG, INFLUXDB_CLOUD_BUCKET, INFLUXDB_CLOUD_TOKEN, InfluxDbCloud2CACert);
 // InfluxDB client instance for self-hosted v1 database
-InfluxDBClient client_v1;
+InfluxDBClient client;
 
-// because we use the deepsleep pattern we put all our logic in the setup() routine and then send the microcontroller back to sleep
-//  if your microcontroller is always up you might want to put the commands in your loop() routine instead
-void setup() {
-  Serial.begin(115200);
-  Serial.println("Starting setup");
-  delay(100);
-  wifiConnect(WIFI_SSID, WIFI_KEY);
-  // BEGIN: read sensor values (add your real code here)
-  float sensor_value1 = 1.23;
-  float sensor_value2 = 0;
-  // END: read sensor values
-
-  Point pointDevice("mymeasurement"); // create a new measurement point (the same point can be used for Cloud and v1 InfluxDB)
-  // add tags to the datapoints so you can filter them
-  pointDevice.addTag("device", DEVICE_ID);
-  pointDevice.addTag("SSID", WiFi.SSID());
-  // Add data fields (values)
-  pointDevice.addField("sensor1", sensor_value1);
-  pointDevice.addField("sensor2", sensor_value2);
-  pointDevice.addField("uptime", millis()); // in addition send the uptime of the Arduino
-  
-  Serial.print("written to InfluxDB Cloud: ");
-  Serial.println(client_cloud.writePoint(pointDevice)); // returns true if success, false otherwise
-
-  client_v1.setConnectionParamsV1(INFLUXDB_URL, INFLUXDB_DATABASE, INFLUXDB_USER, INFLUXDB_PASSWORD);
-  Serial.print("written to local InfluxDB instance: ");
-  Serial.println(client_v1.writePoint(pointDevice)); // returns true if success, false otherwise
-
-  // this sends the microcontroller to deepsleep until the next reading needs to be taken
-  //  due to WiFi connect and sensor reading your measurement interval will always be 5~10 seconds longer than the SLEEP_S duration
-  ESP.deepSleep(SLEEP_S * 1000000 - millis()*1000); // offset by the duration the program run (converted from ms to µs)
-}
-
-void loop() {
-}
-
-// try to connect to given SSID and key, loop until successful
 void wifiConnect(const char* ssid, const char* key) {
   WiFi.begin(ssid, key);
   Serial.print("Waiting for WiFi connection..");
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
-    delay(500);
+    delay(2000);
   }
   Serial.println(".");
   Serial.print("Successfully connected to ");
   Serial.println(WiFi.SSID());
+
+}
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Starting setup");
+  delay(100);
+  wifiConnect(WIFI_SSID, WIFI_KEY);
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+
+  int val_temp_linear;
+  int dat_temp_linear;
+  
+  Serial.print("Moisture Sensor Value:");
+  sensor_moisture = analogRead(0);
+  sensor_water_level = analogRead(1);
+  Serial.println(sensor_moisture);
+  Serial.print("Water Level Value:");
+  Serial.println(sensor_water_level);
+  tmp_temperature = analogRead(2);//Connect LM35 on Analog 2
+  sensor_temperature = (500 * tmp_temperature) /1024;
+  
+  Point pointDevice("sensor");
+  pointDevice.addTag("device", DEVICE_ID);
+  pointDevice.addTag("SSID", WiFi.SSID());
+  
+  pointDevice.addField("Moisture Sensor Value", sensor_moisture);
+  pointDevice.addField("Water level", sensor_water_level);
+  pointDevice.addField("Temperature", sensor_temperature);
+  
+  Serial.print("Written to InfluxDB Cloud: ");
+  Serial.println(client_cloud.writePoint(pointDevice));
+
+  client.setConnectionParamsV1(INFLUXDB_URL, INFLUXDB_DATABASE, INFLUXDB_USER, INFLUXDB_PASSWORD);
+  Serial.print("Written to local InfluxDB instance: ");
+  Serial.println(client.writePoint(pointDevice));
+
+  ESP.deepSleep(SLEEP_S * 1000000 - millis()*1000); // offset by the duration the program run (converted from ms to µs)
 }
