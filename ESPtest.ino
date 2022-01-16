86e696bc6eccb9acdada9eba706fcb267dff4e28
jujucollos <68745924+jujucollos@users.noreply.github.com>
2022-01-16 19:27:08 +0100

    Update ESPtest.ino
    [ESP]: add sensor and wifi

--- ESPtest.ino
+++ ESPtest.ino
@@ -1,73 +1,107 @@
 #if defined(ESP32)
-  #include <WiFiMulti.h>
-  WiFiMulti wifiMulti;
-  #define DEVICE "ESP32"
+#include <WiFiMulti.h>
+WiFiMulti wifiMulti;
+#define DEVICE "ESP32"
 #elif defined(ESP8266)
-  #include <ESP8266WiFiMulti.h>
-  ESP8266WiFiMulti wifiMulti;
-  #define DEVICE "ESP8266"
+#include <ESP8266WiFiMulti.h>
+ESP8266WiFiMulti wifiMulti;
+#define DEVICE "ESP8266"
 #endif
-#include <InfluxDbClient.h> // load the client library
-#include <InfluxDbCloud.h> // only for InfluxDB Cloud: load SSL certificate and additional method call
-#include "secrets.h"
 
-#define SLEEP_S 30
-#define DEVICE_ID "MyRoom"
+#include <InfluxDbClient.h>
+#include <InfluxDbCloud.h>
 
-// InfluxDB client for InfluxDB Cloud API
-InfluxDBClient client_cloud(INFLUXDB_CLOUD_URL, INFLUXDB_CLOUD_ORG, INFLUXDB_CLOUD_BUCKET, INFLUXDB_CLOUD_TOKEN, InfluxDbCloud2CACert);
-// InfluxDB client instance for self-hosted v1 database
-InfluxDBClient client;
+#include <InfluxDbCloud.h>
+#define INFLUXDB_URL "https://europe-west1-1.gcp.cloud2.influxdata.com/"
+#define INFLUXDB_TOKEN "m8PkkqgADBq8p7xNaOlkMVnWxT7lzkUT5zQ_APe5Mz4D4RzthGK0-BBxYfCFxdyPrYvY56T-udVgLHPUgRxJrA=="
+#define INFLUXDB_ORG "563de256efdb04f9"
+#define INFLUXDB_BUCKET "MyData"
 
-void wifiConnect(const char* ssid, const char* key) {
-  WiFi.begin(ssid, key);
-  Serial.print("Waiting for WiFi connection..");
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(2000);
-  }
-  Serial.println(".");
-  Serial.print("Successfully connected to ");
-  Serial.println(WiFi.SSID());
+// WiFi AP SSID
+#define WIFI_SSID "TCOM"
+// WiFi password
+#define WIFI_PASSWORD "<3Stephan<3"
+#define TZ_INFO "CET-1CEST,M3.5.0,M10.5.0/3"
 
-}
+
+
+InfluxDBClient client(INFLUXDB_URL, INFLUXDB_ORG, INFLUXDB_BUCKET, INFLUXDB_TOKEN, InfluxDbCloud2CACert);
+
+
+// Data point
+Point sensor("wifi_status");
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("Starting setup");
-  delay(100);
-  wifiConnect(WIFI_SSID, WIFI_KEY);
-  pinMode(led, OUTPUT);
+  
+  // Setup wifi
+  WiFi.mode(WIFI_STA);
+  wifiMulti.addAP(WIFI_SSID, WIFI_PASSWORD);
+
+  Serial.print("Connecting to wifi");
+  while (wifiMulti.run() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+
+  // Add tags
+  sensor.addTag("device", DEVICE);
+  sensor.addTag("SSID", WiFi.SSID());
+  timeSync(TZ_INFO, "pool.ntp.org", "time.nis.gov");
+
+  // Check server connection
+  if (client.validateConnection()) {
+    Serial.print("Connected to InfluxDB: ");
+    Serial.println(client.getServerUrl());
+  } else {
+    Serial.print("InfluxDB connection failed: ");
+    Serial.println(client.getLastErrorMessage());
+  }
 }
 
 void loop() {
+  // Store measured value into point
+  sensor.clearFields();
+  // Report RSSI of currently connected network
+  sensor.addField("rssi", WiFi.RSSI());
+  // Print what are we exactly writing
+  Serial.print("Writing: ");
+  Serial.println(client.pointToLineProtocol(sensor));
+  // If no Wifi signal, try to reconnect it
+  if (wifiMulti.run() != WL_CONNECTED) {
+    Serial.println("Wifi connection lost");
+  }
+  // Write point
+  if (!client.writePoint(sensor)) {
+    Serial.print("InfluxDB write failed: ");
+    Serial.println(client.getLastErrorMessage());
+  }
 
-  int val_temp_linear;
-  int dat_temp_linear;
-  
-  Serial.print("Moisture Sensor Value:");
-  sensor_moisture = analogRead(0);
-  sensor_water_level = analogRead(1);
-  Serial.println(sensor_moisture);
-  Serial.print("Water Level Value:");
-  Serial.println(sensor_water_level);
-  tmp_temperature = analogRead(2);//Connect LM35 on Analog 2
-  sensor_temperature = (500 * tmp_temperature) /1024;
-  
+  float moisture = analogRead(34);
+  Serial.print("Moisture: ");
+  Serial.print(moisture);
+
+  float water_level = analogRead(32);
+  Serial.print("Water level: ");
+  Serial.print(water_level);
+
+  float humidity = analogRead(33);
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+
   Point pointDevice("sensor");
-  pointDevice.addTag("device", DEVICE_ID);
+  pointDevice.addTag("device", DEVICE);
   pointDevice.addTag("SSID", WiFi.SSID());
   
-  pointDevice.addField("Moisture Sensor Value", sensor_moisture);
-  pointDevice.addField("Water level", sensor_water_level);
-  pointDevice.addField("Temperature", sensor_temperature);
+  pointDevice.addField("Moisture Sensor Value",moisture);
+  pointDevice.addField("Water level", water_level);
+  pointDevice.addField("Humidity", humidity);
   
   Serial.print("Written to InfluxDB Cloud: ");
-  Serial.println(client_cloud.writePoint(pointDevice));
-
-  client.setConnectionParamsV1(INFLUXDB_URL, INFLUXDB_DATABASE, INFLUXDB_USER, INFLUXDB_PASSWORD);
-  Serial.print("Written to local InfluxDB instance: ");
   Serial.println(client.writePoint(pointDevice));
 
-  ESP.deepSleep(SLEEP_S * 1000000 - millis()*1000); // offset by the duration the program run (converted from ms to µs)
+  //Wait 5s
+  Serial.println("Wait 5s");
+  delay(5000);
 }
